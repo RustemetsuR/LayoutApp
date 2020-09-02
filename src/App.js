db09c73324c602ca3600bf902a0c28d63e4c3e34
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-09-02 21:08:56 +0600

    Added Layout,Switch and Route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout/Layout';
+import Page from './containers/Page/Page';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Layout>
+    <Switch>
+      <Route path="/about" component={Page} />
+      <Route path="/contacts" component={Page} />
+      <Route path="/divisions" component={Page} />
+      <Route path="/admin" component={Page} />
+      <Route path="/home" exact component={Page} />
+    </Switch>
+  </Layout>
+);
 
 export default App;
